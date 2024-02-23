638fd511d9a38958db80ed0cf208ad0a21e998fc
Gigi Breton <gigibreton@MacBook-Air.local>
2024-02-23 16:53:55 -0500

    Added error message to prevent pending request

--- src/quiz/basic-server.js
+++ src/quiz/basic-server.js
@@ -45,6 +45,10 @@ const server = http.createServer((req, res) => {
             res.end(JSON.stringify(incidents.filter((incident) => {
                 return incident.id === `MABOS00${id}`
             })));
+        } else {
+            // If not all parameters are found, send an error reponse
+            res.writeHead(400, { 'Content-Type': 'text/plain' });
+            res.end('Invalid parameters in the URL');
         }
     } else {
         // If the request is for an unsupported endpoint, send a not found response
