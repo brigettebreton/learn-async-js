be7887372ee86ffce11221cfedc21f3b632fcda4
Gigi Breton <gigibreton@MacBook-Air-2.local>
2024-03-01 19:56:14 -0500

    In class activities

--- src/fetch-products-async-await.js
+++ src/fetch-products-async-await.js
@@ -1,18 +1,25 @@
+//This entire function runs asynchronusly
+//Defining a function with async makes it return a promise
 async function fetchProducts() {
     console.log('Begin fetch ... ');
         // after this line, our function will wait for the `fetch()` call to be settled
         // the `fetch()` call will either return a Response or throw an error
     const response = await fetch(
+        //await fetch returns a promise. doesn't return immediately,
+        //only returns after request is compelete
         "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
     );
+    //This all only get executed after fetch completes
     console.log('Fetch done');
     if (!response.ok) {
         throw new Error(`HTTP error: ${response.status}`);
     }
     // after this line, our function will wait for the `response.json()` call to be settled
     // the `response.json()` call will either return the parsed JSON object or throw an error
+    //This cannot complete until first fetch completes
     const data = await response.json();
     console.log('response created');
+    //We are returning a promsise with this value in the resolve
     return data[0].name;
   }
   
